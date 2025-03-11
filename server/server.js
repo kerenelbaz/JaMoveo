fbf07a31f4265be93ef040b32ecff2f14f0edb8b
kerenelbaz <132551849+kerenelbaz@users.noreply.github.com>
2025-03-11 15:58:53 +0200

    database
    create database using mongoDB

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require("express")
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors")
 const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: "./config.env" }); //loading environment variables
 const port = process.env.PORT || 3001;
 app.use(cors({ origin: "http://localhost:3000" }));
 
@@ -15,6 +15,14 @@ const io = new Server(server, { cors: { origin: "*" } });
 
 app.get("/", (req, res) => {
     res.send("Server is running")
+});
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB!");
+}).catch(err => {
+    console.error("MongoDB connection error:", err);
 });
 
 
