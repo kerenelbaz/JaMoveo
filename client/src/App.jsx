d45d9a250d754fb5bc5f88484147ed8eef89b863
kerenelbaz <132551849+kerenelbaz@users.noreply.github.com>
2025-03-12 12:29:31 +0200

    socket
    created socket io

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,63 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import socket from './socket'
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import './components/Register'
 import Register from './components/Register'
+import Login from './components/Login'
+import MainPage from './components/MainPage'
+import MainPageAdmin from './components/MainPageAdmin'
+import AdminLogin from './components/AdminLogin'
+import Welcome from './components/welcome'
 
 function App() {
   // const [count, setCount] = useState(0)
+  const [islogged, setIsLogged] = useState(false);
+  const [usernameLogged, setUsernameLogged] = useState("");
+
+  useEffect(() => {
+    if (!islogged || !usernameLogged) {
+      console.log("⚠️ User is not logged in. Skipping socket connection.");
+      return;
+    }
+    console.log("🔵 Emitting user_connected:", usernameLogged);
+    socket.connect();
+    socket.emit("user_connected", usernameLogged);
+    socket.on("disconnect", () => {
+      console.log("❌ Disconnected from server");
+    });
+    return () => {
+      console.log("ℹ️ Cleaning up socket connection...");
+      if (!islogged) {
+        socket.disconnect();
+      }
+    };
+  }, [islogged, usernameLogged]);
+
 
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Welcome />} />
+          <Route path="/login" element={<Login setIsLogged={setIsLogged} setUsernameLogged={setUsernameLogged} />} />
+
+          <Route path='/admin-login' element={<AdminLogin setIsLogged={setIsLogged} setUsernameLogged={setUsernameLogged} />} />
+
+          <Route path="/register" element={<Register setIsLogged={setIsLogged} />} />
+
+          <Route path="/main" element={islogged ? <MainPage /> : <Navigate to="/login" />} />
+
+          <Route path="/main-admin" element={islogged ? <MainPageAdmin /> : <Navigate to="/admin-login" />} />
+        </Routes>
+      </Router>
       {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Socket.IO cilent</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-       */}
-       <Register />
+      <Register /> */}
     </>
   )
 }
--- server/models/User.js
+++ server/models/User.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose")
 
 const UserSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    username: 
-    { 
-        type: String, 
-        required: true, 
-        unique: true 
+    username:
+    {
+        type: String,
+        required: true,
+        unique: true
     },
     password: {
-         type: String, 
-         required: true 
+        type: String,
+        required: true
     },
     instrument:
     {
